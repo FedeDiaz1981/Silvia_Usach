e4c80c3a9cefcf0f81fce16bee4112de1f0e108c
FedeDiaz1981 <federicodiaz1981@gmail.com>
2025-09-15 05:01:00 -0300

    arreglando navegación

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server', // Servidor con páginas estáticas prerenderizadas
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   integrations: [preact()],
 });
